e7868753e55b20d2c8ea6d8fcd811f6aa36c37f2
AnubhabMukherjee2003 <antucruse621@gmail.com>
2025-03-29 14:24:10 +0530

    minor changes

--- app.js
+++ app.js
@@ -60,6 +60,8 @@ app.get("/make-admin", ensureMember, powerController.getAdmin);
 app.post("/make-admin", ensureMember, powerController.validateAdmin, powerController.postAdmin);
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Change this in your app.js file
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
